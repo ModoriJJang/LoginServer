ddb48c8399d422114487462b1e3bc4d1c5d8944e
김태훈 <taehunkim.dev@gmail.com>
2024-01-08 18:55:54 +0900

    LoginController 추가

---
+++ TH_LoginServer/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace TH_LoginServer.Controllers
+{
+
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [Route( "api/[controller]/SignIn" )]
+        [HttpPost]
+        public IActionResult SignIn([FromBody] SignInData data)
+        {
+            if(data.ID == "testID" && data.PW == "testPW")
+            return Ok( "OK" );
+
+            return BadRequest("FAIL");
+        }
+
+        [Route( "api/[controller]/SignUp" )]
+        [HttpPost]
+        public IActionResult SignUp([FromBody] SignUpData data)
+        {
+            return Ok( "" );
+        }
+
+        [Route( "api/[controller]/ReSign" )]
+        [HttpPost]
+        public IActionResult ReSign()
+        {
+            return Ok( "" );
+        }
+
+        public class SignInData()
+        {
+            public string ID { get; set; } = string.Empty;
+            public string PW {get;set;} = string.Empty;
+        }
+
+        public class SignUpData
+        {
+            public string ID {get;set;} = string.Empty;
+            public string PW {get;set;} = string.Empty;
+        }
+    }
+}
